44de829ce54b16f5232bcbfcbe5bb20965a65d16
Duc-khanh <nguyenkhanh2561990@gmail.com>
2025-08-25 15:04:13 +0700

    first commit

---
+++ src/main/java/com/example/football_field_management/FootballFieldManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.football_field_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFieldManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballFieldManagementApplication.class, args);
+    }
+
+}
