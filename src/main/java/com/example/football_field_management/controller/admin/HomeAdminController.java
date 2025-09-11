03a7aea76ae024c287b5fbf20cbe391681856d50
kieutandung <tandungkieu1312@gmail.com>
2025-09-11 09:56:34 +0700

    update header,account manage

--- src/main/java/com/example/football_field_management/controller/admin/HomeAdminController.java
+++ src/main/java/com/example/football_field_management/controller/admin/HomeAdminController.java
@@ -1,5 +1,8 @@
 package com.example.football_field_management.controller.admin;
 
+import com.example.football_field_management.model.Account;
+import com.example.football_field_management.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -10,12 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class HomeAdminController {
+    private final AccountRepository accountRepo;
     @GetMapping("/homeAdmin")
     public String home(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("Authorities on /admin/homeAdmin: " + auth.getAuthorities());
-        model.addAttribute("username", auth.getName());
+        String username = auth.getName();
+
+        Account account;
+        if (username.contains("@")) {
+            account = accountRepo.findByEmail(username).orElse(null);
+        } else {
+            account = accountRepo.findByPhone(username).orElse(null);
+        }
+
+        if (account != null) {
+            model.addAttribute("fullName", account.getFullName());
+            model.addAttribute("emailOrPhone", account.getEmail() != null ? account.getEmail() : account.getPhone());
+            model.addAttribute("avatar", account.getAvt_path() != null ? account.getAvt_path() : "/images/avatar.png");
+        }
+
         return "admin/home";
     }
+
 }
