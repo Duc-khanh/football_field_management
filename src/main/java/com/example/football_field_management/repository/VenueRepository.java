8eeff127a0b6d6a27339bef71badf3afa41b22b5
kieutandung <tandungkieu1312@gmail.com>
2025-11-12 19:02:50 +0700

    beta ui

--- src/main/java/com/example/football_field_management/repository/VenueRepository.java
+++ src/main/java/com/example/football_field_management/repository/VenueRepository.java
@@ -4,6 +4,7 @@ import com.example.football_field_management.model.Venue;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,9 @@ public interface VenueRepository extends JpaRepository<Venue,Long> {
 
     Page<Venue> findByVenueNameContainingIgnoreCaseAndStatus(String keyword, Boolean status, Pageable pageable);
 
+    @Query("SELECT v FROM Venue v JOIN OrderPayment op ON op.id = v.owner.account_id " +
+            "WHERE op.status = 'COMPLETE' " +
+            "GROUP BY v.venueId " +
+            "ORDER BY COUNT(op.id) DESC")
+    List<Venue> findTop5ByCompletedOrders(Pageable pageable);
 }
